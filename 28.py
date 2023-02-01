cf1da1836048e57730b9c4d090e945cd2c4d74d5
wxchz <120259302+wxchz@users.noreply.github.com>
2023-02-01 05:02:51 -0800

    Update 28.py

--- 28.py
+++ 28.py
@@ -13,4 +13,6 @@ while True:
             Fahrenheit= float(input("Enter temperature in Fahrenheit: "))
             Kelvin = 5/9 * (Fahrenheit - 32) + 273.15
             print("The temperature in Kelvin is:",round(Kelvin,2))
-            sys.exit()
+            chk=input("Press E to Exit or Enter to continue:  ")
+            if((chk=='E') or (chk=='e')):
+                sys.exit()
