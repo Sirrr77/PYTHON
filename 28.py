08fe1ccb5c6a6181462a94db844118d6a8a4b9e4
wxchz <120259302+wxchz@users.noreply.github.com>
2023-02-01 04:04:30 -0800

    Add files via upload

---
+++ 28.py
@@ -0,0 +1,16 @@
+#แปลงค่าอุณหภูมิจากหน่วยฟาเรนไฮต์เป็นหน่วยเคลวิน
+
+import sys
+
+while True:
+     pwd=input("input password  ")
+     if pwd !="skw": #รหัสผ่าน
+         print("Incorrect password try again pls: ") #ถ้ารหัสถูกจะรันโค้กให้
+     else:
+        print("Correct Password Ready to Execute \n") #ถ้ารหัสผิดจะให้ใส่ไหม่
+
+        while True:
+            Fahrenheit= float(input("Enter temperature in Fahrenheit: "))
+            Kelvin = 5/9 * (Fahrenheit - 32) + 273.15
+            print("The temperature in Kelvin is:",round(Kelvin,2))
+            sys.exit()
